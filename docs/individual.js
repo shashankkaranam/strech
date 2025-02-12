95135cf62675139252e3f5628533c32318399652
shashankkaranam <156595130+shashankkaranam@users.noreply.github.com>
2025-02-12 22:24:21 +0530

    Update individual.js

--- docs/individual.js
+++ docs/individual.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 
 // Middleware for parsing form data
 app.use(express.urlencoded({ extended: true }));
